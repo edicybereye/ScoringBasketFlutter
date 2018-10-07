59eebc88a59887c9b6a32ef1be739cbcfcaf9cae
whaapps <edi.cybereye@gmail.com>
2018-10-07 14:51:54 +0700

    release

---
+++ lib/main.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int scoreA = 0, scoreB = 0;
+
+  // int scoreA = 0;
+  // int scoreB = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Text(
+                      "$scoreA",
+                      style: TextStyle(fontSize: 30.0),
+                    ),
+                    RaisedButton(
+                      color: Colors.blue,
+                      onPressed: () {
+                        setState(() {
+                          scoreA++;
+                        });
+                      },
+                      child: Text(
+                        'Tambah 1',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    RaisedButton(
+                      color: Colors.blue,
+                      onPressed: () {
+                        setState(() {
+                          scoreA += 2;
+                        });
+                      },
+                      child: Text(
+                        'Tambah 2',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    RaisedButton(
+                      color: Colors.blue,
+                      onPressed: () {
+                        setState(() {
+                          scoreA += 3;
+                        });
+                      },
+                      child: Text(
+                        'Tambah 3',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    )
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    Text(
+                      "$scoreB",
+                      style: TextStyle(fontSize: 30.0),
+                    ),
+                    RaisedButton(
+                      color: Colors.blue,
+                      onPressed: () {
+                        setState(() {
+                          scoreB++;
+                        });
+                      },
+                      child: Text(
+                        'Tambah 1',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    RaisedButton(
+                      color: Colors.blue,
+                      onPressed: () {
+                        setState(() {
+                          scoreB += 2;
+                        });
+                      },
+                      child: Text(
+                        'Tambah 2',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    RaisedButton(
+                      color: Colors.blue,
+                      onPressed: () {
+                        setState(() {
+                          scoreB += 3;
+                        });
+                      },
+                      child: Text(
+                        'Tambah 3',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+            RaisedButton(
+              color: Colors.blue,
+              onPressed: () {
+                setState(() {
+                  scoreA = 0;
+                  scoreB = 0;
+                });
+              },
+              child: Icon(
+                Icons.send,
+                color: Colors.white,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
